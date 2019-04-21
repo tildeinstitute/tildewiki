37dc7439cea09d4897830adaa6978fc995a84d14
Ben Morrison <ben@gbmor.dev>
2019-04-21 00:21:18 -0400

    split files

---
+++ handlers.go
@@ -0,0 +1,47 @@
+package main
+
+import "net/http"
+
+func viewHandler(w http.ResponseWriter, r *http.Request, filename string) {
+	p, err := loadPage(filename)
+	if err != nil {
+		http.Redirect(w, r, "/edit/"+filename, http.StatusFound)
+		return
+	}
+	renderTemplate(w, "view", p)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request, filename string) {
+	p, err := loadPage(filename)
+	if err != nil {
+		p, err := loadPage("template")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		p.Filename = filename
+	}
+	renderTemplate(w, "edit", p)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request, filename string) {
+	body := r.FormValue("body")
+	filename = r.FormValue("filename") + ".md"
+	p := &Page{Filename: filename, Body: []byte(body)}
+	err := p.save()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w, r, "/"+filename, http.StatusFound)
+}
+
+func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		m := validPath.FindStringSubmatch(r.URL.Path)
+		if m == nil {
+			http.NotFound(w, r)
+			return
+		}
+		fn(w, r, m[2])
+	}
+}
---
+++ pages.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bufio"
+	"html/template"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"regexp"
+	"strings"
+
+	"github.com/russross/blackfriday"
+)
+
+var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
+var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+
+// Page holds wiki page title and body
+type Page struct {
+	Filename string
+	Title    string
+	Body     []byte
+}
+
+func (p *Page) save() error {
+	return ioutil.WriteFile(p.Filename, p.Body, 0600)
+}
+
+func loadPage(filename string) (*Page, error) {
+	if filename == "" {
+		filename = "wiki.md"
+	} else {
+		filename += ".md"
+	}
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	title := getTitle(filename)
+	parsed := blackfriday.Run(body)
+	return &Page{Filename: filename, Title: title, Body: parsed}, nil
+}
+
+func getTitle(filename string) string {
+	mdfile, err := os.Open(filename)
+	if err == nil {
+		titlefinder := bufio.NewScanner(mdfile)
+		for titlefinder.Scan() {
+			splitter := strings.Split(titlefinder.Text(), ":")
+			if splitter[0] == "title" {
+				return strings.TrimSpace(splitter[1])
+			}
+		}
+	}
+	return filename
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	err := templates.ExecuteTemplate(w, tmpl+".html", p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
