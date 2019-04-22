b5912890328e7017bc0d17c67ff50444439524f2
Ben Morrison <ben@gbmor.dev>
2019-04-22 01:28:14 -0400

    too many m:n thread spawn points

--- main.go
+++ main.go
@@ -3,17 +3,11 @@ package main // import "github.com/gbmor/tildewiki"
 import (
 	"log"
 	"net/http"
-	"runtime"
 )
 
 func main() {
-	runtime.GOMAXPROCS(runtime.NumCPU())
-	go func() {
-		http.HandleFunc("/", welcomeHandler)
-	}()
-	go func() {
-		http.HandleFunc("/w/", makeHandler(viewHandler))
-	}()
+	http.HandleFunc("/", welcomeHandler)
+	http.HandleFunc("/w/", makeHandler(viewHandler))
 	//http.HandleFunc("/edit/", makeHandler(editHandler))
 	//http.HandleFunc("/save/", makeHandler(saveHandler))
 	log.Fatal(http.ListenAndServe(":8080", nil))
