345d4b57479b4267abb71db2ae2ee77fe56831ac
Ben Morrison <ben@gbmor.dev>
2019-04-25 03:11:03 -0400

    removed dead code

--- assets.go
+++ assets.go
@@ -5,7 +5,6 @@ import (
 	_ "image/gif"
 	_ "image/jpeg"
 	_ "image/png"
-	"io"
 	"log"
 	"mime"
 	"os"
@@ -13,30 +12,20 @@ import (
 	"github.com/spf13/viper"
 )
 
-// Guess image format from gif/jpeg/png/webp
-func guessImageFormat(r io.Reader) (format string, err error) {
-	_, format, err = image.DecodeConfig(r)
-	return
-}
-
-// Guess image mime types from gif/jpeg/png/webp
-func guessImageMimeTypes(r io.Reader) string {
-	format, _ := guessImageFormat(r)
-	if format == "" {
-		return ""
-	}
-	return mime.TypeByExtension("." + format)
-}
-
 // open the icon file and process it
 func iconType(filename string) string {
-	r, err := os.Open(filename)
+	file, err := os.Open(filename)
+	defer file.Close()
 	if err != nil {
 		log.Println("iconType() :: Couldn't open icon, sending mime type image/unknown")
 		return "image/unknown"
 	}
-	defer r.Close()
-	mime := guessImageMimeTypes(r)
+	_, format, err := image.DecodeConfig(file)
+	if err != nil {
+		log.Println("iconType() :: Can't decode icon, sending mime type image/unknown")
+		return "image/unknown"
+	}
+	mime := mime.TypeByExtension("." + format)
 	log.Println("iconType() :: " + mime)
 	return mime
 }
