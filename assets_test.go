ac929272c412e5ba803e511fd4d73b38dca95670
Ben Morrison <ben@gbmor.dev>
2019-04-30 03:30:42 -0400

    eliminated need for iconType()

--- assets_test.go
+++ assets_test.go
@@ -20,26 +20,6 @@ func Test_setUpUsTheWiki(t *testing.T) {
 	}
 }
 
-func Test_iconType(t *testing.T) {
-	type args struct {
-		filename string
-	}
-	tests := []struct {
-		name string
-		args args
-		want string
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := iconType(tt.args.filename); got != tt.want {
-				t.Errorf("iconType() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
 var cssLocalTests = []struct {
 	name []byte
 	want bool
