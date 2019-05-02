b10ec7f9882857b7f37c90180d2d3fc1eaec4194
Ben Morrison <ben@gbmor.dev>
2019-05-02 15:31:48 -0400

    finished test/bench of render()

--- md_test.go
+++ md_test.go
@@ -1,28 +1,34 @@
 package main
 
 import (
+	"io/ioutil"
 	"reflect"
 	"testing"
 
 	bf "github.com/gbmor-forks/blackfriday.v2-patched"
 )
 
+var mdTestData1, _ = ioutil.ReadFile("pages/example.md")
+var mdTestData2, _ = ioutil.ReadFile("pages/test1.md")
 var markdownTests = []struct {
 	name  string
 	css   string
 	title string
+	data  []byte
 	want  *bf.HTMLRenderer
 }{
 	{
 		name:  "one",
 		css:   "assets/wiki.css",
 		title: "test page 1",
+		data:  mdTestData1,
 		want:  bf.NewHTMLRenderer(bf.HTMLRendererParameters{}),
 	},
 	{
 		name:  "two",
 		css:   "https://tilde.institute/tilde.css",
 		title: "test page 2",
+		data:  mdTestData1,
 		want:  bf.NewHTMLRenderer(bf.HTMLRendererParameters{}),
 	},
 }
@@ -46,8 +52,21 @@ func Benchmark_setupMarkdown(b *testing.B) {
 	}
 }
 
-// wrapper function to generate the parameters above and
-// pass them to the blackfriday library's parsing function
-//func Test_render(t *testing.T) {
-//return bf.Run(data, bf.WithRenderer(setupMarkdown(css, title)))
-//}
+func Test_render(t *testing.T) {
+	for _, tt := range markdownTests {
+		t.Run(string(tt.name), func(t *testing.T) {
+			var got interface{}
+			got = render(tt.data, tt.css, tt.title)
+			if _, ok := got.([]byte); !ok {
+				t.Errorf("render() didn't return byte array: %v", reflect.TypeOf(got))
+			}
+		})
+	}
+}
+func Benchmark_render(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, c := range markdownTests {
+			render(c.data, c.css, c.title)
+		}
+	}
+}
