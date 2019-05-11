95330ef5549db50639b790315404b0137335b5e5
Ben Morrison <ben@gbmor.dev>
2019-05-11 01:36:03 -0400

    moved log setup to init(), using channels to signal to close log file, catching interrupt sig

--- main.go
+++ main.go
@@ -4,51 +4,40 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"time"
 
 	"github.com/gorilla/handlers"
 )
 
 // TildeWiki version
-const twvers = "0.6.0"
+const twvers = "0.6.2"
+
+// Makes the deferred close functions for the log file
+// block until exit
+var closelog = make(chan bool, 1)
 
 func main() {
 
-	// show the logo, repo link, etc
-	setUpUsTheWiki()
+	// watch for SIGINT aka ^C
+	// close the log file then exit
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
 
-	// initialize the configuration
-	initConfigParams()
+	go func() {
+		for sigint := range c {
+			log.Printf("\n\nCaught %v. Cleaning up ...\n", sigint)
 
-	// set up logging if the config file params
-	// are set
-	if confVars.fileLogging {
-		if llogfile, err := os.OpenFile(confVars.logFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0600); err == nil {
-			log.SetOutput(llogfile)
-			defer func() {
-				err := llogfile.Close()
-				if err != nil {
-					log.Printf("Couldn't close log file: %v\n", err)
-				}
-			}()
-		} else {
-			log.Printf("Couldn't log to file: %v\n", err)
+			if confVars.fileLogging {
+				// signal to close the log file
+				closelog <- true
+				time.Sleep(50 * time.Millisecond)
+			}
+
+			close(closelog)
+			os.Exit(0)
 		}
-	}
-	// Tell Tildewiki to be quiet,
-	// Supersedes file logging
-	if confVars.quietLogging {
-		if llogfile, err := os.Open("/dev/null"); err == nil {
-			log.SetOutput(llogfile)
-			defer func() {
-				err := llogfile.Close()
-				if err != nil {
-					log.Printf("Couldn't close log file: %v\n", err)
-				}
-			}()
-		} else {
-			log.Printf("Couldn't quiet logging: %v\n", err)
-		}
-	}
+	}()
 
 	// fill the page cache
 	log.Println("**NOTICE** Building initial cache ...")
@@ -71,4 +60,10 @@ func main() {
 	}
 
 	log.Fatal(http.ListenAndServe(confVars.port, handlers.CompressHandler(serv)))
+
+	// signal to close the log file
+	if confVars.fileLogging || confVars.quietLogging {
+		closelog <- true
+		close(closelog)
+	}
 }
