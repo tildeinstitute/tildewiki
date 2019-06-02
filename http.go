169165d23ff5c267aa5db2b70131dd8f89c21a75
Ben Morrison <ben@gbmor.dev>
2019-06-02 03:05:21 -0400

    logging IP and user-agent for requests/errors

--- http.go
+++ http.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net"
 	"net/http"
@@ -39,4 +41,68 @@ func ipMiddleware(hop http.Handler) http.Handler {
 		ctx := newCtxUserIP(r.Context(), r)
 		hop.ServeHTTP(w, r.WithContext(ctx))
 	})
+}
+
+func log200(r *http.Request) {
+	useragent := r.Header["User-Agent"]
+	uip := getIPfromCtx(r.Context())
+	log.Printf("**** %v :: 200 :: %v %v :: %v\n", uip, r.Method, r.URL, useragent)
+}
+
+// wrapper for testing 500 pages via /500
+func error500(w http.ResponseWriter, r *http.Request) {
+	log500(w, r, fmt.Errorf("500 Page Accessed Directly, No Error"))
+}
+
+// this is a custom 500 page using a markdown doc
+// in the assets directory.
+// if the markdown doc can't be read, default to
+// net/http's error handling
+func log500(w http.ResponseWriter, r *http.Request, topErr error) {
+
+	useragent := r.Header["User-Agent"]
+	uip := getIPfromCtx(r.Context())
+	log.Printf("**** %v :: 500 :: %v %v :: %v :: %v\n", uip, r.Method, r.URL, useragent, topErr.Error())
+
+	confVars.mu.RLock()
+	e500 := confVars.assetsDir + "/500.md"
+	confVars.mu.RUnlock()
+
+	file, err := ioutil.ReadFile(e500)
+	if err != nil {
+		log.Printf("Tried to read 500.md: %v\n", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", htmlutf8)
+	_, err = w.Write(render(file, "500: Internal Server Error"))
+	if err != nil {
+		log.Printf("Failed to write to HTTP stream: %v\n", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+// this is a custom 404 page using a markdown doc
+// in the assets directory.
+// if the markdown doc can't be read, default to
+// net/http's error handling
+func error404(w http.ResponseWriter, r *http.Request) {
+	confVars.mu.RLock()
+	e404 := confVars.assetsDir + "/404.md"
+	confVars.mu.RUnlock()
+
+	file, err := ioutil.ReadFile(e404)
+	if err != nil {
+		log.Printf("Tried to read 404.md: %v\n", err)
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+
+	w.Header().Set("Content-Type", htmlutf8)
+	_, err = w.Write(render(file, "404: Not Found"))
+	if err != nil {
+		log.Printf("Failed to write to HTTP stream: %v\n", err)
+		error500(w, r)
+	}
 }
